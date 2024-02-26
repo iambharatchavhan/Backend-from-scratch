e1bd03347d5845a25eae198bcd3eea7119eb9e10
iambharatchavhan <bharatchavhan141@gmail.com>
2024-02-26 19:22:12 +0530

    logn route added

--- AuthJwt/routes/userRoutes.js
+++ AuthJwt/routes/userRoutes.js
@@ -2,11 +2,12 @@ const express= require('express')
 const router = express.Router()
 
 const userSignUp = require('../controller/singUp')
+const login = require('../controller/login')
 
 
 
 router.post('/sign-up', userSignUp);
-
+router.post('/login',login)
 
 
 
